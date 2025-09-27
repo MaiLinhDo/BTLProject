42893506a426cdc3f213bb21b61a95484f161dc4
MaiLinhDo <domailinh4020@gmail.com>
2025-09-27 11:19:02 +0700

    add folder api

---
+++ LaptopStore/app/config.py
@@ -0,0 +1,12 @@
+import os
+
+class Config:
+    DB_SERVER = os.getenv("DB_SERVER", "DESKTOP-A9RVON6\\SQLEXPRESS")
+    DB_DATABASE = os.getenv("DB_DATABASE", "LaptopStore")
+
+    SQL_SERVER_CONN = (
+        f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+        f"SERVER={DB_SERVER};"
+        f"DATABASE={DB_DATABASE};"
+        f"Trusted_Connection=yes;"
+    )
